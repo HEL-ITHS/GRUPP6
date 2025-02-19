7a777ca1d4138f6cc5e21768be5570647556dc77
Lukas Rönnholm - FEU24 GBG <lukas.ronnholm@iths.se>
2025-02-19 10:16:32 +0100

    Påbörjat quiz kompontent

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
     export default {
         data() {
             return {
@@ -26,4 +26,51 @@
         <li v-for="word in words" > {{ word.english }} - {{ word.swedish }}</li>
     </ul>
 
+</template> -->
+<script>
+export default{
+    data(){
+        return{
+            showAnswer: false,
+            quiz: [
+            {
+                "question": "Vad är hej på engelska?",
+                "answers": {
+                "a": "Hello",
+                "b": "Goodbye",
+                "c": "Thanks"
+                },
+                "correctAnswer": "Hello"
+            }
+        ]
+    }
+    },
+    methods:{
+        show() {
+            this.showAnswer = true
+            console.log("show")
+
+        }
+    }
+}
+
+
+
+</script>
+
+<template>
+<div>
+    <h3> {{ quiz[0].question }}</h3>
+    <form>
+       <input type="radio">{{ quiz[0].answers.a }}
+       <input type="radio">{{ quiz[0].answers.b }}
+       <input type="radio"> {{ quiz[0].answers.c }}
+       <input type="submit" value="Svara" @click="show()">
+    </form>
+    <p v-if="showAnswer">{{ quiz[0].correctAnswer }}</p>
+    
+
+</div>
+ 
 </template>
+

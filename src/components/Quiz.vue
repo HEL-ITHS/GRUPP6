13b4d56135862f60c159059d65f4df83ecd83417
linushallin <linus.hallin@outlook.com>
2025-02-20 12:29:37 +0100

    tog bort gammal kod

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,32 +1,4 @@
-<!-- <script>
-    export default {
-        data() {
-            return {
-                words: [],
-            }
-        },
-        methods: {
-            getWords() {
-                fetch("/public/words.json")
-                .then(response => response.json())
-                .then(data => {
-                    this.words = data.words
-                    console.log(data)
-                })
-            }
-        },
-        created() {
-            this.getWords()
-        }
-    }
-</script>
 
-<template>
-    <ul>
-        <li v-for="word in words" > {{ word.english }} - {{ word.swedish }}</li>
-    </ul>
-
-</template> -->
 <script>
 export default{
     data(){

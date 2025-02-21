b24755148fbb76f0ae7e778a26d38a5597ed8ccb
Lukasronnholm <bengtssonronnholm@gmail.com>
2025-02-21 14:51:00 +0100

    Logging och länkar samt bootstrap

--- src/Views/LogInView.vue
+++ src/Views/LogInView.vue
@@ -3,21 +3,25 @@ export default {
   data() {
     return {
       value: "",
+      email: "",
+      password: "",
     };
   },
-  /* methods: {
-    onClick() {
-      if (this.value !== "") {
-      this.$router.push()
-      }
-    },
-  }, */
+
   methods: {
     onClick() {
-      if (this.value === "student") {
-        router.push("/student");
-      } else if (this.value === "teacher") {
-        router.push("/teacher");
+      if (
+        this.value === "student" &&
+        this.email !== "" &&
+        this.password !== ""
+      ) {
+        this.$router.push("/student");
+      } else if (
+        this.value === "teacher" &&
+        this.email !== "" &&
+        this.password !== ""
+      ) {
+        this.$router.push("/teacher");
       }
     },
   },
@@ -25,19 +29,39 @@ export default {
 </script>
 
 <template>
-  <form>
-    <input placeholder="Namn" required />
-    <input placeholder="Lösenord" type="password" required />
-    <select>
-      <option selected disabled>Välj roll</option>
-      <option :value="student">
-        <RouterLink to="/student"> Elev </RouterLink>
-      </option>
-      <option :value="teacher">
-        <RouterLink to="/teacher"> Lärare </RouterLink>
-      </option>
-    </select>
-    <button @click="onClick()">Logga in</button>
+  <form @submit.prevent="onClick()" class="container mt-5">
+    <div class="mb-3">
+      <label for="email" class="form-label"><strong>Email:</strong></label>
+      <input
+        v-model="email"
+        id="email"
+        class="form-control"
+        type="email"
+        required
+      />
+    </div>
+    <div class="mb-3">
+      <label for="password" class="form-label"
+        ><strong>Lösenord:</strong></label
+      >
+      <input
+        v-model="password"
+        id="password"
+        class="form-control"
+        type="password"
+        required
+      />
+    </div>
+    <div class="mb-3">
+      <label for="typeOfUser" class="form-label"><strong>Välj:</strong></label>
+      <select v-model="value" id="typeOfUser" class="form-select" required>
+        <option selected disabled value="">Välj</option>
+        <option value="student">Elev</option>
+        <option value="teacher">Lärare</option>
+      </select>
+    </div>
+    <div class="d-flex justify-content-center mb-3">
+      <button type="submit" class="btn btn-primary">Logga in</button>
+    </div>
   </form>
-  <div></div>
 </template>

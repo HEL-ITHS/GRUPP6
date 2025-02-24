31bcf8ccc20451c8c6f3291fb9046f737ea83293
hel-ux <helena.frenell@gmail.com>
2025-02-24 11:23:58 +0100

    Footer gjordes och lades till i App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,21 @@
 import LogInView from "./Views/LogInView.vue";
 import StudentView from "./Views/StudentView.vue";
 import TeacherView from "./Views/TeacherView.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
     LogInView,
     StudentView,
     TeacherView,
+    Footer
   },
 };
 </script>
 
 <template>
-  
+
   <RouterView />
+  <Footer />
 
 </template>
-

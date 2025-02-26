0e0f1c92108523722c8c7473fdbc81f3c3398d1c
Elgoteborge <robin.n.olsson@outlook.com>
2025-02-26 10:14:57 +0100

    Skapat en svårighetsgrad-vy

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
 import LogInView from "./Views/LogInView.vue";
 import StudentView from "./Views/StudentView.vue";
 import TeacherView from "./Views/TeacherView.vue";
+import DifficultyView from "./Views/DifficultyView.vue";
 import FooterAll from "./components/FooterAll.vue";
+
 
 export default {
   components: {
     LogInView,
     StudentView,
+    DifficultyView,
     TeacherView,
     FooterAll
   },

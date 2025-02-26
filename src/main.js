c3ef4a61ae0ec49ee2f683d74d8e72cbfcb25dd4
Heléna <helena.frenell@iths.se>
2025-02-26 18:32:23 +0100

    Implementerade Pinia

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia"
+
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 
 import router from "./router";
 import App from "./App.vue";
 
-createApp(App).use(router).mount("#app");
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+
+app.mount("#app")
+
+/* createApp(App).use(router).mount("#app"); */

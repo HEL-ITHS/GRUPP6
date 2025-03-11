48ffd13ab6b29d90429c7b32ca2d1ed6f1ba2f4f
hel-ux <helena.frenell@gmail.com>
2025-03-11 13:13:30 +0100

    Tog bort kommentar

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia"
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
-import router from "./router";
-import App from "./App.vue";
+import router from './router'
+import App from './App.vue'
 
 const pinia = createPinia()
 const app = createApp(App)
@@ -13,6 +13,4 @@ const app = createApp(App)
 app.use(pinia)
 app.use(router)
 
-app.mount("#app")
-
-/* createApp(App).use(router).mount("#app"); */
+app.mount('#app')
